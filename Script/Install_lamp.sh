6038fcb091bb3479422aa1f98ffcc131caaf9724
Marina Tamaral <tamaralgonzalezm@gmail.com>
2025-10-08 07:34:50 +0000

    creamos el script y el index php

---
+++ Script/Install_lamp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+set -e
+
+# Actualizamos repositorios
+ apt update
+ apt upgrade -y
+# Instalamos Apache
+ apt install apache2 -y
+# Instalamos PhP
+ apt install php libapache2-mod-php php-mysql -y
+# Instalamos MySQL
+ apt install mysql-server -y
+# Reiniciamos Apache
+ systemctl restart apache2
+ #copiamos nuestro archivo de prueba php a /var/www/html
+    cp ../php/index.php /var/www/html
